7f0344e8253f7db00a0ae278b4b39d1288f6280f
Jan Gerling <jan.gerling@posteo.de>
2020-08-14 13:31:41 +0200

    Fix: refactoring type specified twice in config

--- configs.py
+++ configs.py
@@ -135,7 +135,6 @@ CLASS_LEVEL_REFACTORINGS = ["Extract Class",
                             "Move Class",
                             "Rename Class",
                             "Introduce Polymorphism",
-                            "Move And Rename Class",
                             "Convert Anonymous Class To Type"]
 
 METHOD_LEVEL_REFACTORINGS = ["Extract And Move Method",
