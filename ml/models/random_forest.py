66a186d53e086ef7a0470c3b8f5b211ad91b7576
Jan Gerling <jan.gerling@posteo.de>
2020-08-26 11:01:09 +0200

    Configured a fast random forest classifier to speed up the training

--- ml/models/random_forest.py
+++ ml/models/random_forest.py
@@ -7,7 +7,7 @@ from ml.models.base import SupervisedMLRefactoringModel
 class RandomForestRefactoringModel(SupervisedMLRefactoringModel):
     def params_to_tune(self):
         return {"max_depth": [3, 6, 12, 24, None],
-                  "max_features": ["auto", "sqrt", "log2", None],
+                  "max_features": ["auto", "log2", None],
                   "min_samples_split": [2, 3, 4, 5, 10],
                   "bootstrap": [True, False],
                   "criterion": ["gini", "entropy"],
@@ -26,3 +26,27 @@ class RandomForestRefactoringModel(SupervisedMLRefactoringModel):
         return RandomForestClassifier(random_state=42)
 
 
+class RandomForestFastRefactoringModel(SupervisedMLRefactoringModel):
+    """
+    A faster simpler configuration of the random forest classifier. This will return quick results, with (hopefully) slightly reduced accuracy.
+    """
+    def params_to_tune(self):
+        return {"max_depth": [3, 12, 24],
+                "max_features": ["auto", "log2"],
+                "min_samples_split": [2, 5, 10],
+                "bootstrap": [True, False],
+                "criterion": ["gini"],
+                "n_estimators": [10, 100, 200]}
+
+
+    def model(self, best_params=None):
+        if best_params is not None:
+            return RandomForestClassifier(random_state=42, n_jobs=CORE_COUNT,
+                                          max_depth=best_params["max_depth"],
+                                          max_features=best_params["max_features"],
+                                          min_samples_split=best_params["min_samples_split"],
+                                          bootstrap=best_params["bootstrap"],
+                                          criterion=best_params["criterion"],
+                                          n_estimators=best_params["n_estimators"])
+
+        return RandomForestClassifier(random_state=42)
